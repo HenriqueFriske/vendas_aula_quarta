7e15ea65f2ca1151a338a471d698448a9b9ef408
HenriqueFriske <henriquekulbabafriske@gmail.com>
2025-04-23 21:29:56 -0300

    Adiciona preço unitário e cálculo de total por produto

--- vendas.py
+++ vendas.py
@@ -3,10 +3,13 @@ vendas = []
 while True:
     produto = input("Nome do produto: ")
     quantidade = int(input("Quantidade vendida: "))
+    preco_unitario = float(input("Preço unitário: "))
 
     vendas.append({
         "produto": produto,
-        "quantidade": quantidade
+        "quantidade": quantidade,
+        "preco_unitario": preco_unitario,
+        "total": quantidade * preco_unitario
     })
 
     cont = input("Deseja continuar? (s/n): ")
@@ -15,4 +18,4 @@ while True:
 
 print("\n--- Vendas Realizadas ---")
 for venda in vendas:
-    print(f"{venda['produto']} - {venda['quantidade']} unidades")
+    print(f"{venda['produto']} - {venda['quantidade']} unidades - R${venda['preco_unitario']:.2f} cada - Total: R${venda['total']:.2f}")
